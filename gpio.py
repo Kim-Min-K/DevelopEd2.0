df5ae635164d638238f458191546b940d32a480b
Logan Omelchuk <logan@omelchuk.me>
2023-09-16 14:20:07 -0600

    Inprogress gpio

--- gpio.py
+++ gpio.py
@@ -1,4 +1,33 @@
 import RPi.GPIO as GPIO
 
-def ReadInput()
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+#Set Button and LED pins
+twistSwitch = 26
+blueSwitch = 26
+redSwitch = 26
+greenSwitch = 26
+yellowSwitch = 26
+clickSwitch = 26
+
+GPIO.setup(twistSwitch,GPIO.IN,pull_up_down=GPIO.PUD_UP)
+GPIO.setup(blueSwitch,GPIO.IN,pull_up_down=GPIO.PUD_UP)
+GPIO.setup(redSwitch,GPIO.IN,pull_up_down=GPIO.PUD_UP)
+GPIO.setup(greenSwitch,GPIO.IN,pull_up_down=GPIO.PUD_UP)
+GPIO.setup(yellowSwitch,GPIO.IN,pull_up_down=GPIO.PUD_UP)
+GPIO.setup(clickSwitch,GPIO.IN,pull_up_down=GPIO.PUD_UP)
+
+def getInput(value):
     
+    if GPIO.input(value) == GPIO.HIGH:
+        state = False
+    else:
+        state = True
+    return state
+
+def updateState():
+    pushBlue = getInput(blueSwitch)
+    pushGreen = getInput(greenSwitch)
+    pushRed = getInput(redSwitch)
+    pushBlue = getInput(blueSwitch)
+    switchBlue =                                            
