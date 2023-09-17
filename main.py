c7d3ab14c744e5c1cddc8486d1e29ab226596e41
CBlackduck <cblackduck@live.ca>
2023-09-17 10:45:45 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,8 @@
 import pygame
 import os
-import pygame_menu
 import pygame.font
-from pygame_menu import themes
 import random
 
-def main_menu():
-    while True:
-        print("hello")
 
 def main():
     pygame.init()
@@ -15,31 +10,13 @@ def main():
     display_surface = pygame.display.set_mode((1280, 720))
     pygame.display.set_caption("Menu")
 
-    # Menu Page 
-    font = pygame.font.Font('freesansbold.ttf',100)
-    text = font.render("Welcome!",True, (255,255,255))
-    textRect = text.get_rect()
-    textRect.center = (200,200)
+    display_surface = pygame.display.get_surface()
 
-    display_surface.blit(text, (640,360))
-    pygame.display.update()
-
-
-    w_surface = pygame.display.get_surface()
-
-    game = Game(w_surface)
-
-    main_menu()
+    game = Game(display_surface)
 
     game.play()
 
     pygame.quit()
-
-
-
-
-def homeScreen():
-    pass 
 
 
 class Game:
@@ -48,14 +25,23 @@ class Game:
 
         self.surface = surface
         self.bg_color = pygame.Color("black")
-
-        self.fps = 1
+        self.font_color = pygame.Color("white")
+        self.fps = 60
         self.game_clock = pygame.time.Clock()
         self.close_clicked = False
         self.continue_game = True
+        self.button_streak = 0
+
+        self.highlighted = 0
+        self.highlighted_color = "red"
+        self.difficulty = "Easy"
+        self.difficulty_set = False
+        self.start_game = False
+        # start = list[0], easy = list[1], medium = list[2], hard = list[3]
+        # used when drawing text
+        self.highlighted_list = [self.highlighted_color,"black","black","black"]
 
         self.buttons = []
-
         self.red_button = Red_Button(surface)
         self.buttons.append(self.red_button)
         self.blue_button = Blue_Button(surface)
@@ -73,43 +59,121 @@ class Game:
         self.time_left = 60.0
 
     def play(self):
-        while not self.close_clicked:
+        while True:
+            if not self.start_game:     # have not started the game yet
+                self.main_menu()
             self.handle_events()
             self.draw()
             if self.continue_game:
+                if not self.difficulty_set:
+                    self.set_difficulty()
                 self.update()
                 self.decide_continue()
+            else:                              # game ended, restart the game
+                self.continue_game = True
+                self.start_game = False
+            self.game_clock.tick(self.fps)
+    
+    def handle_events(self):
+        events = pygame.event.get() # needs to be called or crashes
+
+    def draw(self):
+        self.surface.fill(self.bg_color)
+        self.red_button.draw()
+        self.draw_time()
+        pygame.display.update()
+
+    def main_menu(self):
+        while not self.start_game:
+            self.menu_handler()
+            self.draw_menu()
             self.game_clock.tick(self.fps)
 
-    def handle_events(self):
+    def menu_handler(self):
         events = pygame.event.get()
         for event in events:
             if event.type == pygame.QUIT:
                 self.close_clicked = True
+            elif event.type == pygame.KEYDOWN:  ############ REMOVE for actual implementation
+                if event.key == pygame.K_p:        # enter pressed  
 
-    def draw(self):
+                    if self.highlighted == 0:
+                        self.start_game = True
+                    elif self.highlighted == 1:
+                        self.difficulty = "Easy"
+                    elif self.highlighted == 2:
+                        self.difficulty = "Medium"
+                    else:
+                        self.difficulty = "Hard"
+
+                elif event.key == pygame.K_w:                         # down pressed
+                    for i in range(len(self.highlighted_list)):
+                        self.highlighted_list[i] = "black"
+                    self.highlighted -= 1
+                    if self.highlighted == -1:                             # looped around
+                        self.highlighted = 3
+                    self.highlighted_list[self.highlighted] = self.highlighted_color
+
+                elif event.key == pygame.K_s:                         # up pressed
+                    for i in range(len(self.highlighted_list)):       # set all colors to black
+                        self.highlighted_list[i] = "black"
+                    self.highlighted += 1
+                    self.highlighted = self.highlighted % 4                # looped around
+                    self.highlighted_list[self.highlighted] = self.highlighted_color   # set word to highlight
+
+    # enter once per game to set difficulty variables
+    def set_difficulty(self):
+        self.difficulty_set = True
+        if self.difficulty == "Easy":
+            self.time_left = 60.0
+        elif self.difficulty == "Medium":
+            self.time_left = 45.0
+        elif self.difficulty == "Hard":
+            self.time_left = 5.0
+
+    def draw_menu(self):
         self.surface.fill(self.bg_color)
-        random.choice(self.buttons).draw()
-        self.draw_time()
-        pygame.display.update()
+        font_size = 80
+        font = pygame.font.SysFont("",font_size)  
 
+        start = font.render("Start",True,self.font_color,self.highlighted_list[0]) 
+        easy = font.render("Easy",True,self.font_color,self.highlighted_list[1]) 
+        medium = font.render("Medium",True,self.font_color,self.highlighted_list[2]) 
+        hard = font.render("Hard",True,self.font_color,self.highlighted_list[3]) 
+        current_difficulty = font.render("Current Difficulty: "+ self.difficulty,True,self.font_color,self.bg_color) 
+
+        self.surface.blit(start,(300,120)) 
+        self.surface.blit(easy,(300,250)) 
+        self.surface.blit(medium,(300,380)) 
+        self.surface.blit(hard,(300,510)) 
+        self.surface.blit(current_difficulty,(300,640))   
+
+        pygame.display.update()
+            
     def draw_time(self):
         font_size = 80
-        font_color = pygame.Color("white")
         font = pygame.font.SysFont("",font_size)
-        timer_text = font.render(str(self.time_left),True,font_color,self.bg_color)
+        timer_text = font.render(str(round(self.time_left,1)),True,self.font_color,self.bg_color)
+        streak_text = font.render(str(self.button_streak),True,self.font_color,self.bg_color)
 
         timer_location = (1100,0)
         self.surface.blit(timer_text,timer_location)
+        self.surface.blit(streak_text,(0,0))
+
 
     def update(self):
-        self.time_left -= 1
+        self.time_left -= 1/60
         
 
     def decide_continue(self):
-        pass
+        if self.time_left < 0:   # add an input to go back to start, auto does it atm
+            self.surface.fill(self.bg_color)
+            self.continue_game = False
+            self.difficulty_set = False
+            self.difficulty = "Easy"
 
 
+    
 class Red_Button():
 
     def __init__(self,surface):
